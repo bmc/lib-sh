219fb3580c2d4bf66b85684a2157b2ec28e7e551
Brian Clapper <bmc@clapper.org>
2003-07-09 14:35:31 +0000

    Initial recasts of original KSH files

---
+++ bclapper.fulltilt.com.sh
@@ -0,0 +1,8 @@
+# $Id$
+
+export FTP_PASSIVE_MODE=on
+export FTP_PASSIVE=true		# for perl
+export CVSEDITOR=`type -P vi`
+
+alias sdrive="smbclient //ftmaster/share -U bclapper -I ftmaster.fulltilt.com"
+
---
+++ sunball.sh
@@ -0,0 +1,71 @@
+# ---------------------------------------------------------------------------
+# Settings and overrides specific to host "raft"
+#
+# $Id$
+# ---------------------------------------------------------------------------
+
+# ---------------------------------------------------------------------------
+# Misc. environment variables
+
+export PERL5LIB=/usr/local/lib/perl
+export www=/usr/local/www/apache
+export APACHE_HOME=$www
+export TOMCAT_HOME=$APACHE_HOME/tomcat
+
+# ---------------------------------------------------------------------------
+# Java-related environment variables
+
+export JAVA_ROOT=/usr/local/java
+export ANT_HOME=/usr/local/java/ant
+
+if [ -n "$JAVA_HOME" ]
+then
+    rmpath PATH "$JAVA_ROOT/jdk*/bin"
+else
+    export JAVA_HOME=/usr/local/java/jdk
+fi
+
+eval `classpath -k -J`
+
+# ---------------------------------------------------------------------------
+# PATH
+
+export PATH=\
+$PATH:\
+$JAVA_HOME/bin:\
+/usr/local/site/sbin:\
+/usr/local/sbin:\
+/usr/sbin:\
+/sbin:\
+$ORACLE_HOME/bin:\
+$ANT_HOME/bin
+
+# ---------------------------------------------------------------------------
+# Aliases and functions
+
+alias camf="check-alt-mail-folders"
+alias ftp=ncftp
+alias phoenix="/usr/local/phoenix/phoenix"
+alias mllog="sudo tail -f /var/log/maillog"
+alias mslog="sudo tail -f /var/log/messages"
+alias nslookup="$(type -P nslookup) -silent"
+#alias play="/usr/lib/oss/play"
+alias www='varcd www'
+alias xinit="$XWINHOME/bin/xinit -- -bpp 16"
+
+# ---------------------------------------------------------------------------
+# Development stuff
+
+load_file ~/bash/development.sh
+
+# ---------------------------------------------------------------------------
+# X stuff
+
+# Where have I logged in from?
+
+where=`login-from`
+case "$where" in
+    z|z.inside.clapper.org)
+        xdisplay $where
+	;;
+esac
