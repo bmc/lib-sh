5f1f66fe40974094d7b523b4ce5f87dcd29e6fce
Brian Clapper <bmc@clapper.org>
2003-07-09 14:33:32 +0000

    Added /usr/local/site/bin to PATH

--- inside.clapper.org.sh
+++ inside.clapper.org.sh
@@ -7,4 +7,6 @@ export CVSEDITOR=`type -P vi`
 export PERL5LIB="/usr/local/lib/perl:/usr/local/site/lib/perl:$HOME/lib/perl"
 cleanpath PERL5LIB
 
+export PATH=/usr/local/site/bin:$PATH
+
 alias camf=check-alt-mail-folders
