0692cfcff2f85ac24bc21adacdc3936f82c8beb0
Brian Clapper <bmc@clapper.org>
2003-11-12 16:49:38 +0000

    Added "photos" alias.

--- inside.clapper.org.sh
+++ inside.clapper.org.sh
@@ -8,6 +8,9 @@ export PERL5LIB="/usr/local/lib/perl:/usr/local/site/lib/perl:$HOME/lib/perl"
 cleanpath PERL5LIB
 
 export PATH=/usr/local/site/bin:$PATH
+export public=/usr/local/site/public
+export photos=$public/photos
 
 alias camf=check-alt-mail-folders
-alias photos="cd /usr/local/site/public/photos"
+alias photos="varcd photos"
+alias public="varcd public"
