faf13bd585d40dedf45ec64e6de73bf27432a9fb
Brian Clapper <bmc@clapper.org>
2004-07-15 01:35:09 +0000

    Added some definitions

--- inside.clapper.org.sh
+++ inside.clapper.org.sh
@@ -8,11 +8,20 @@ export PERL5LIB="/usr/local/lib/perl:/usr/local/site/lib/perl:$HOME/lib/perl"
 cleanpath PERL5LIB
 
 export PATH=/usr/local/site/bin:$PATH
+
 export public=/usr/local/site/public
+alias public="varcd public"
+
 export photos=$public/photos
+alias photos="varcd photos"
+
 export mystuff=/usr/local/src/my-stuff/working
+alias mystuff="varcd mystuff"
+
+export wsrc="$mystuff"
+alias wsrc='varcd wsrc'
+
+export jsrc="$mystuff/java"
+alias jsrc='varcd jsrc'
 
 alias camf=check-alt-mail-folders
-alias photos="varcd photos"
-alias public="varcd public"
-alias mystuff="varcd mystuff"
