83907a26f7d0ba633d6e73061ba735893269296b
Brian Clapper <bmc@clapper.org>
2006-01-07 14:44:57 +0000

    Added CATALINA_HOME

--- condor.sh
+++ condor.sh
@@ -5,6 +5,7 @@ export www='/usr/local/site/www/apache'
 export wwwtest="$www/wwwtest"
 export CVSROOT=/usr/local/src/my-stuff/CVS-tree
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/site/lib
+export CATALINA_HOME=/usr/local/jakarta-tomcat5.0
 
 export PATH=\
 $PATH:\
