e5438610c8e175fc80f1779d953cee356372651f
Brian Clapper <bmc@clapper.org>
2006-01-20 16:34:33 +0000

    Renamed beardfoot.sh to rasta-yore.sh, and made some changes.

--- rasta-yore.sh
+++ rasta-yore.sh
@@ -11,11 +11,6 @@ else
 fi
 #export EMACS_SHOW_TIME="yes"
 
-export NETHACKOPTIONS='pickup_types:$?+!=/'
-
-export www='/usr/local/site/www/apache'
-export wwwtest="$www/wwwtest"
-
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/site/lib
 
 # ---------------------------------------------------------------------------
@@ -28,19 +23,13 @@ $PATH:\
 /usr/local/site/sbin:\
 /usr/local/sbin:\
 /usr/sbin:\
-/sbin:\
-/usr/local/Acrobat4/bin:\
-$HOME/OpenOffice
+/sbin:
 
 # ---------------------------------------------------------------------------
 # Local aliases and functions
 
-alias frm="$(type -P frm) -S"
 alias ftp=ncftp3
 alias lynx=links
 alias vi=vim
-alias xinit="$XWINHOME/bin/xinit -- -bpp 16"
-alias www='varcd www'
-alias wwwtest='varcd wwwtest'
 alias resize='/usr/X11R6/bin/resize'
 alias xcolorsel="echo 'try colordemo, instead'"
