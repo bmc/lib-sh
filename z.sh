c9b1007734b6b54c931de2d6edb44a8ef52527d6
Brian Clapper <bmc@clapper.org>
2006-01-26 12:16:51 +0000

    updated

--- z.sh
+++ z.sh
@@ -1,64 +1,28 @@
-# $Id$
-
 # ---------------------------------------------------------------------------
-# Environment variables
-
-if [ -z $PERL5LIB ]
-then
-    export PERL5LIB=/usr/local/lib/perl
-else
-    export PERL5LIB=$PERL5LIB:/usr/local/lib/perl
-fi
-export MAKE_BASE="/usr/local/src/mystuff/working/libs"
-export MAKE_CFG="$MAKE_BASE/make"
-#export EMACS_SHOW_TIME="yes"
-
-export CVSROOT=/usr/local/src/my-stuff/CVS-tree
-export NETHACKOPTIONS='pickup_types:$?+!=/'
-
+# Settings and overrides specific to host "z"
+# ---------------------------------------------------------------------------
 export www='/usr/local/site/www/apache'
 export wwwtest="$www/wwwtest"
-
+export CVSROOT=/usr/local/src/my-stuff/CVS-tree
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/site/lib
 
-# ---------------------------------------------------------------------------
-# PATH
+export EDITOR=vim
+export VISUAL=$EDITOR
 
 export PATH=\
 $PATH:\
-/usr/X11R6/bin:\
-/usr/local/site/bin:\
 /usr/local/site/sbin:\
 /usr/local/sbin:\
 /usr/sbin:\
 /sbin:\
-/usr/local/Acrobat4/bin:\
-$HOME/OpenOffice
+/usr/X11R6/bin:\
+/usr/local/spamassassin/bin
 
-# ---------------------------------------------------------------------------
-# Local X stuff
-
-export XMCD_LIBDIR=/usr/local/X11/lib/xmcd
-
-# ---------------------------------------------------------------------------
-# Local aliases and functions
+# Local aliases
 
 alias frm="$(type -P frm) -S"
-alias ftp=ncftp3
-alias lynx=links
-alias mplayerxp="mplayerxp -xp -double"
-alias mplayer=gmplayer
+alias ftp=ncftp
 alias play="/usr/local/lib/oss/play"
 alias vi=vim
-alias xinit="$XWINHOME/bin/xinit -- -bpp 16"
 alias www='varcd www'
 alias wwwtest='varcd wwwtest'
-alias resize='/usr/X11R6/bin/resize'
-alias xcolorsel="echo 'try colordemo, instead'"
-
-# ---------------------------------------------------------------------------
-# Development stuff
-
-export ANT_HOME=/usr/local/ant
-load_file ~/bash/java.sh
-#load_file ~/bash/fulltilt.sh
