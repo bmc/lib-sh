02f3b4a7a1da48fb708c311178e2591edab32669
Brian Clapper <bmc@clapper.org>
2006-11-21 04:22:59 +0000

    Initial Mac OS X versions.

---
+++ darkroom.sh
@@ -0,0 +1,96 @@
+# ---------------------------------------------------------------------------
+# Settings and overrides specific to host "darkroom"
+#
+# $Id: sunball.sh 6539 2006-11-02 14:45:36Z bmc $
+# ---------------------------------------------------------------------------
+
+# ---------------------------------------------------------------------------
+# Misc. environment variables
+
+# ---------------------------------------------------------------------------
+
+export photos="/Volumes/CLAPPER-ORG;CONDOR-1"
+export PERL5LIB=$HOME/lib/perl
+
+# ---------------------------------------------------------------------------
+# Java-related environment variables
+
+export JAVA_ROOT=/usr/local/java
+export ANT_HOME=/usr/local/java/ant
+
+if [ -n "$JAVA_HOME" ]
+then
+    rmpath PATH "$JAVA_ROOT/jdk*/bin"
+else
+    export JAVA_HOME=/usr/local/java/jdk
+fi
+
+eval `classpath -k -J`
+
+# ---------------------------------------------------------------------------
+# PATH
+
+export PATH=\
+$HOME/bin:\
+$PATH:\
+/sw/bin
+
+# ---------------------------------------------------------------------------
+# Aliases and functions
+
+alias blog='varcd blog'
+alias camf="check-alt-mail-folders"
+#alias dfspace="dfspace -t ext3 -t ext2"
+alias ftp=ncftp
+alias mllog="sudo tail -f /var/log/maillog"
+alias mslog="sudo tail -f /var/log/messages"
+alias nslookup="$(type -P nslookup) -silent"
+
+# Only necessary if using OSS drivers. FC6 sound drivers seem to work.
+#alias play="/usr/lib/oss/bin/ossplay"
+alias www='varcd www'
+alias xinit="/usr/bin/xinit -- -bpp 16"
+
+# ---------------------------------------------------------------------------
+# Development stuff
+
+export mystuff=$HOME/src/mystuff
+alias mystuff="varcd mystuff"
+
+export jsrc="$mystuff/java"
+alias jsrc='varcd jsrc'
+
+export ANT_HOME=$JAVA_ROOT/ant
+export MAVEN_HOME=$JAVA_ROOT/maven
+export GROOVY_HOME=$JAVA_ROOT/groovy
+
+PATH=\
+$PATH:\
+$ANT_HOME/bin:\
+$MAVEN_HOME/bin:\
+$GROOVY_HOME/bin:\
+$HOME/Eclipse/eclipse
+
+load_file ~/bash/java.sh
+load_file ~/bash/fulltilt.sh
+
+# ---------------------------------------------------------------------------
+# OSS
+
+PATH=$PATH:/usr/lib/oss/sbin:/usr/lib/oss/bin # ADDED_BY_OSS
+SDL_DSP_NOSELECT=1 # ADDED_BY_OSS
+export PATH SDL_DSP_NOSELECT # ADDED_BY_OSS
+
+# ---------------------------------------------------------------------------
+# Local stuff
+
+# Change public directory, then reload domain stuff
+
+export usr_local_site=/mnt/condor/usr/local/site
+export public=${usr_local_site}/public
+load_file ~/bash/$DOMAIN.sh
+
+export wwwtest="${usr_local_site}/www/apache/wwwtest"
+alias wwwtest='varcd wwwtest'
+
+export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:/usr/local/lib
---
+++ osx.sh
@@ -0,0 +1,89 @@
+# $Id: freebsd.sh 1719 2005-03-26 02:35:49Z bmc $
+
+# ---------------------------------------------------------------------------
+# Local variables
+
+# `console_vt_pattern' defines the device name pattern for virtual terminals
+# on the console.  `.login' uses it and `console_term' to set the default
+# terminal type.  If the O/S doesn't support virtual terminals from the
+# console, `console_vt_pattern' should be empty.
+console_vt_pattern="ttyv[0-9]"
+console_term=cons25
+
+# ---------------------------------------------------------------------------
+# Environment variables
+
+# Path setting
+export PATH=/sw/bin:$PATH:/usr/sbin:/sbin 
+
+export MAIL=/var/mail/$USER
+
+if [[ -z $?TERM ]]
+then
+    TERM="dumb"
+fi
+export TERM
+
+if [[ "$TERM" == xterm* ]]
+then
+    term xterm
+fi
+
+if [[ -z $PERL5LIB ]]
+then
+    export PERL5LIB=/usr/local/lib/perl
+fi
+
+export PERL5LIB=${PERL5LIB}:$HOME/lib/perl
+export LSCOLORS='3x6x5x2x2x3x3x2x2x3x3x'
+
+unset MANPATH	# /etc/manpath.config takes care of it
+
+# LOGIN_RHOST is set by bashrc
+
+if [ -z "$LOGIN_RHOST" ]
+then
+    # Local login. Check tty. If it's not a pseudo-tty, then assume we're
+    # not running under X, and clear the display.
+
+    case "`tty`" in
+	/dev/ttyp*)
+	    ;;
+	*)
+	    unset DISPLAY
+	    ;;
+    esac
+fi
+
+# ---------------------------------------------------------------------------
+# Aliases and functions
+
+# Assume that all OS X machines have the fileutils port installed.
+eval `dircolors ~/.dir_colors`
+
+_fetch=$(type -P fetch)
+alias fetch="$_fetch -p"
+unset _fetch
+
+alias hping2=hping
+alias ls='/sw/bin/ls --color=tty -CF'
+alias psme='ps -U bmc -ouser,pid,ppid,tty,command'
+alias sha1='digest sha1'
+alias time='/usr/bin/time --portability'
+alias unshar=gunshar
+alias xs='eval `resize -u`'
+alias whois=jwhois
+
+_vipw=`type -P vipw`
+alias vipw='EDITOR=vi VISUAL=vi sudo $_vipw'
+
+function pki
+{
+    pkg_glob -O -a | grep -i $*
+}
+
+function psf
+{
+    ps -ax -opid,ppid,vsz,rss,user,tty,command|egrep "PID|$1"
+}
+
