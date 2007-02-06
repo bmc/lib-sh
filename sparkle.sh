91b6a96697d973dc644dfa19dd381a89328fc730
Brian Clapper <bmc@clapper.org>
2007-02-06 03:49:33 +0000

    new

---
+++ sparkle.sh
@@ -0,0 +1,18 @@
+# ---------------------------------------------------------------------------
+# Settings and overrides specific to host "goff"
+# ---------------------------------------------------------------------------
+export CLASSPATH=""
+
+export PATH=\
+$PATH:\
+/usr/local/site/sbin:\
+/usr/local/sbin:\
+/usr/sbin:\
+/sbin:\
+/usr/X11R6/bin
+
+# Local aliases
+
+alias ftp=ncftp
+alias vi=vim
+
