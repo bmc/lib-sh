4e007001a102288b7a6761f1356f7809aec098cc
Brian Clapper <bmc@clapper.org>
2007-04-17 01:22:34 +0000

    Another change to Java path.

--- darkroom.sh
+++ darkroom.sh
@@ -20,13 +20,28 @@ export NETHACKOPTIONS='pickup_types=$!?=/,gender=male,fruit=kiwi'
 
 export JAVA_ROOT=$HOME/java
 export ANT_HOME=$JAVA_ROOT/ant
-#export JAVA_HOME=/System/Library/Frameworks/JavaVM.framework/Versions/1.6/Home
-export JAVA_HOME=/Library/Java/Home
+
+# According to
+# http://adcdownload.apple.com/Java/java_se_6.0_release_1_developer_preview_6/javase6r1dp6releasenotes.html#Switchable_Command_Line_Tools
+#
+# "Developers should not rely on the existence or behavior of the User
+# Switchable Java Command Line Tools, as they are, by definition, under the
+# control of the user. If it is necessary for an application to use a
+# specific version of Java, the application developer should take
+# appropriate steps to not rely on /usr/bin/java, but rather should use
+# explicit paths into
+# /System/Library/Frameworks/JavaVM.framework/Versions/1.x/Commands for
+# shell script based invocations of Java tools."
+
+export JAVA_HOME=/System/Library/Frameworks/JavaVM.framework/Versions/1.6/Home
+#export JAVA_HOME=/Library/Java/Home
 
 eval `classpath -k -J`
 
 # ---------------------------------------------------------------------------
 # PATH
+
+# On Darwin, use /usr/bin/java, not what's in JAVA_HOME.
 
 export PATH=\
 $JAVA_HOME/bin:\
