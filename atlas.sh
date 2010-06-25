cdfecd6511b877f4095e0f3092a87854de7f53f3
Brian Clapper <bmc@clapper.org>
2010-06-24 22:01:40 -0400

    new machine

---
+++ atlas.sh
@@ -0,0 +1,77 @@
+# ---------------------------------------------------------------------------
+#
+# $Id$
+# ---------------------------------------------------------------------------
+
+# ---------------------------------------------------------------------------
+# Misc. environment variables
+
+export PERL5LIB=$HOME/lib/perl
+export mystuff=$HOME/src/mystuff
+
+export PYTHONPATH="$PYTHONPATH:/var/lib/python-support/python2.6:$HOME/lib/python:$HOME/google_appengine:/usr/share/pyshared"
+
+export FORTUNE_FILE=$HOME/lib/games/fortunes
+
+# ---------------------------------------------------------------------------
+# PATH
+
+export PATH=\
+/usr/bin:\
+/bin:\
+$HOME/python/bin:\
+$PATH:\
+/usr/local/sbin:\
+/usr/sbin:\
+/sbin:
+
+# ---------------------------------------------------------------------------
+# Programmatic Completion
+
+unset -f _get_vpn_completions
+_get_vpn_completions()
+{
+    local cur prev possibles
+    # "cur" is the current word (the one being completed)
+    cur="${COMP_WORDS[COMP_CWORD]}"
+    # prev is the previous word type. We don't use that here.
+    prev="${COMP_WORDS[COMP_CWORD-1]}"
+
+    # COMPREPLY is a bash array in which the responses are returned
+    COMPREPLY=()
+
+    # Generate the list of possible matches (i.e., the directories under
+    # $HOME/openvpn
+    possibles=
+    for f in $HOME/openvpn/*
+    do
+        if [ -d $f ]
+        then
+            possibles="$possibles $(basename $f)"
+        fi
+    done
+
+    # Now, use the compgen built-in to generate the matches
+    COMPREPLY=( $(compgen -W "$possibles" -- $cur) )
+}
+# enable completions on command "vpn"
+complete -F _get_vpn_completions vpn
+
+# ---------------------------------------------------------------------------
+# Aliases and functions
+
+alias ftp=ncftp
+alias mllog="sudo tail -f /var/log/mail.log"
+alias mslog="sudo tail -f /var/log/messages"
+alias nslookup="$(type -P nslookup) -silent"
+alias mystuff="varcd mystuff"
+alias mypy='varcd mypy'
+alias top=htop
+
+# ---------------------------------------------------------------------------
+# Local stuff
+
+export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:/usr/local/lib:$HOME/lib
+
+load_file ~/bash/ubuntu.sh
+
