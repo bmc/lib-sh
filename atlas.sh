6f4742c55177d3438cc757f54c36aed96ff972f3
Brian Clapper <bmc@clapper.org>
2010-11-09 21:25:42 -0500

    Updated to Scala 2.8.1

--- atlas.sh
+++ atlas.sh
@@ -86,6 +86,52 @@ alias photos='varcd photos'
 alias top=htop
 alias www='varcd www'
 
+function switch-scala
+{
+    case $# in
+        0)
+            echo $SCALA_HOME
+            return
+            ;;
+        1)
+            ;;
+        *)
+            echo "Usage: switch-scala scala" >&2
+            return 1
+            ;;
+    esac
+
+    case "$1" in
+        2.7|2.7.?)
+           _n=$HOME/scala/scala-2.7.7
+           ;;
+        2.8|2.8.?|2.8.*|default)
+           _n=$HOME/scala/scala-2.8.1
+           ;;
+        *)
+           ;;
+    esac
+
+    if [ ! -d $_n ]
+    then
+        echo "No such Scala version -- $1 ($_n)" >&2
+        return 1
+    fi
+
+    export PATH=$(echo $PATH | sed "s+$SCALA_HOME/bin:++g")
+    if [ -n "$SCALA_HOME" ]
+    then
+	rmpath PATH $SCALA_HOME/bin
+    fi
+    export SCALA_HOME=$_n
+    PATH=$SCALA_HOME/bin:$PATH
+    echo $SCALA_HOME
+}
+
+alias set-scala=switch-scala
+set-scala default
+
+
 # ---------------------------------------------------------------------------
 # Local stuff
 
