01212b074b8a6e0ba74a72c5cc77cd3f1962537d
Brian M. Clapper <bmc@clapper.org>
2010-12-07 14:41:50 -0500

    BClap_VDI.sh -> BCLAPPER.sh

--- BCLAPPER.sh
+++ BCLAPPER.sh
@@ -1,4 +1,5 @@
 
+export nodosfilewarning=1
 alias vi=vim
 
 pathinsert()
@@ -17,31 +18,25 @@ pathappend()
     done
 }
 
+unset -f mkprompt
+mkprompt()
+{
+    PS1='\[\e[1m\](\h:\u) \W $ \[\e[m\]'
+}
+
 export DISPLAY=127.0.0.1:0.0
 export SCALA_HOME=c:\\scala
 export JAVA_HOME="D:\\Java\\JDK6"
 export FHOME=/cygdrive/f/home/bclapper
 export HOME="/cygdrive/c/Documents and Settings/bclapper"
+export dev="$HOME/development"
+export idw="$dev/code/idw"
 
+pathinsert "$HOME/emacs-22.3/bin"
 pathinsert "$HOME/python/bin"
-pathappend $HOME/emacs-22.3/bin
-pathinsert $JAVA_HOME\\bin
-pathappend $SCALA_HOME\\bin
-pathappend c:\\java\\apache-ant-1.7.1\\bin
-pathinsert $HOME/bin
+pathinsert "$HOME/bin"
 
 alias d='ls -CF --color'
 alias fhome='varcd FHOME'
-
-# Java
-
-wd=$(pwd)
-cd $HOME/java/classes
-CLASSPATH=.
-for i in *
-do
-    CLASSPATH="$CLASSPATH;c:\\$HOMEPATH\\java\\classes\\$i"
-done
-export CLASSPATH
-cd $wd
-unset wd
+alias dev='varcd dev'
+alias idw='varcd idw'
