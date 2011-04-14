e2cd9959d3e77b3f11d13677ccc94863b991e325
Brian Clapper <bmc@clapper.org>
2011-04-14 23:23:03 +0000

    Added ssh agent

--- whizzer.sh
+++ whizzer.sh
@@ -1,1 +1,9 @@
 export TERM=xterm1
+
+SSHAGENT=/usr/bin/ssh-agent
+SSHAGENTARGS="-s"
+if [ -z "$SSH_AUTH_SOCK" -a -x "$SSHAGENT" ]
+then
+    eval `$SSHAGENT $SSHAGENTARGS`
+    trap "kill $SSH_AGENT_PID" 0
+fi
