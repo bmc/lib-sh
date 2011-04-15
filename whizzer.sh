e7270c0c87279ed21d72315c4068276955eb0a6f
Brian Clapper <bmc@clapper.org>
2011-04-15 18:09:26 -0400

    ssh stuff

--- whizzer.sh
+++ whizzer.sh
@@ -1,12 +1,12 @@
+# ---------------------------------------------------------------------------
+# Environment variables
+
 export TERM=xterm1
 
-SSHAGENT=/usr/bin/ssh-agent
-SSHAGENTARGS="-s"
-if [ -z "$SSH_AUTH_SOCK" -a -x "$SSHAGENT" ]
-then
-    eval `$SSHAGENT $SSHAGENTARGS`
-    trap "kill $SSH_AGENT_PID" 0
-    ssh-add -D
-    echo "Adding SSH identity."
-    ssh-add
-fi
+# ---------------------------------------------------------------------------
+# Aliases
+
+# ---------------------------------------------------------------------------
+# Startup stuff
+
+ssha
