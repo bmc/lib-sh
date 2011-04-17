896738e1a29af2fe3de109fad9ee455a67b00710
Brian Clapper <bmc@clapper.org>
2011-04-17 12:50:38 +0000

    Updated alias for mllog

--- whizzer.sh
+++ whizzer.sh
@@ -7,6 +7,8 @@ export TERM=xterm1
 # Aliases
 
 alias top=htop
+alias mllog="sudo tail -f /var/log/mail.log"
+alias mslog="sudo tail -f /var/log/messages"
 
 # ---------------------------------------------------------------------------
 # Startup stuff
