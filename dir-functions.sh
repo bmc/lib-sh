e373073ce4b306907166ec12a114a27cdb2ab5aa
Brian Clapper <bmc@ardentex.com>
2012-01-05 13:49:23 -0500

    More separation of bash and zsh

--- dir-functions.sh
+++ dir-functions.sh
@@ -20,42 +20,6 @@
 # ---------------------------------------------------------------------------
 
 # ---------------------------------------------------------------------------
-# Personal version of cd(1)
-
-alias cd=chdir
-alias up="cd .."
-PROMPT_COMMAND=mkprompt
-
-chdir()
-{
-    _rc=0
-
-    _run_cd_exit_hook
-    if [ $# = 0 ]
-    then
-	builtin cd -P ~
-	if [ $? != 0 ]
-	then
-	    echo "Bad directory: $HOME" >&2
-	    _rc=1
-	fi
-    else
-	builtin cd -P "$*"
-	if [ $? != 0 ]
-	then
-	    #echo "Bad directory: $*" >&2
-	    _rc=1
-	fi
-    fi
-
-    owd=$OLDPWD
-    cwd=$PWD
-    mkprompt
-    _run_cd_entry_hook
-    return $_rc
-}
-
-# ---------------------------------------------------------------------------
 # Manage aliases for directories that are driven from environment variable
 # settings. Aliases that use this function are defined as follows:
 #
